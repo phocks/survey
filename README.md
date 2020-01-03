9bf094f8173a642fd5ce0dbbfba8d7299f74e3bb
Joshua Byrd <phocks@gmail.com>
2020-01-03 16:17:51 +1000

    A readme

---
+++ README.md
@@ -0,0 +1,10 @@
+Survey
+======
+
+A React component that loads an inline survey.
+
+We suggest using `pnpm`
+
+Install with `npm add -g pnpm`
+
+Then run a dev server with `pnpm start`
--- src/index.js
+++ src/index.js
@@ -1,29 +1,40 @@
 import React from "react";
-import { useInputChange } from './useInputChange'
+import { useInputChange } from "./useInputChange";
 
 import styles from "./style.scss";
 
 export default props => {
   const [input, handleInputChange] = useInputChange();
 
-  const handleSubmit = (event) => {
-    console.log("Form submitted...")
+  const handleSubmit = event => {
+    console.log("Form submitted...");
     event.preventDefault();
-  }
+  };
 
   return (
     <div className={styles.root}>
+      <h1>What is your favourite animal?</h1>
       <form onSubmit={handleSubmit}>
-       <div>
-        <label>Username:</label>
-        <input type="radio" name="username" onChange={handleInputChange} />
-      </div>
-      <div>
-        <label>Password:</label>
-        <input type="radio" name="password" onChange={handleInputChange} />
-      </div>
-      <input type="submit" />
-    </form>
+        <div className={styles.radio}>
+          <label for="cat">Cat</label>
+          <input
+            type="radio"
+            name="cat"
+            id="cat"
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className={styles.radio}>
+          <label for="dog">Dog</label>
+          <input
+            type="radio"
+            name="dog"
+            id="dog"
+            onChange={handleInputChange}
+          />
+        </div>
+        {/* <input type="submit" /> */}
+      </form>
       {/* <h2>A test question!!</h2>
       <button
         onClick={() => {
